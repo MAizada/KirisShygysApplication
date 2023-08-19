22af6feda665e0594839da9f50d3265fd391504e
Miras <iskakov>
2023-08-19 18:31:04 +0600

    Initial Commit

---
+++ kirisShygysApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kirisShygysApp
+//
+//  Created by Miras Iskakov on 19.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
